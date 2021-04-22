861227fa411c4ee0f7502498afb97720e6b63865
Isaac <isaacseligcoding@gmail.com>
2021-04-22 12:05:19 +0100

    added_schema

--- models/user.model.js
+++ models/user.model.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
   last_name: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
+  email: { type: String, required: true },
   age: { type: Number, min: 18 },
 });
 
